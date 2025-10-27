1ebb2882b2a8999526586e88616b6fdfa7d82c8f
sepehr.tanhaei <sepehr75t@gmail.com>
2025-10-27 19:05:28 +0100

    fixed formula overflow

--- src/components/Formula.vue
+++ src/components/Formula.vue
@@ -1,17 +1,26 @@
 <script setup>
+import { computed } from "vue";
 import katex from "katex";
 import "katex/dist/katex.min.css";
-import { computed } from "vue";
 
 const props = defineProps({
   tex: { type: String, required: true },
-  inline: { type: Boolean, default: false },
+  inline: { type: Boolean, default: true },
 });
-const html = computed(() => katex.renderToString(props.tex, { throwOnError: false, output: "html", displayMode: !props.inline }));
+
+const html = computed(() =>
+    katex.renderToString(props.tex, {
+      throwOnError: false,
+      output: "html",
+      displayMode: !props.inline,
+    })
+);
 </script>
 
 <template>
-  <span class="badge badge-accent text-black my-2 py-7">
-    <div class="text-[14px]" v-html="html" />
-  </span>
+  <div class="badge badge-accent text-black my-2 p-2  h-full block">
+    <span class="text-[14px]  break-words overflow-wrap" v-html="html"></span>
+  </div>
 </template>
+
+
